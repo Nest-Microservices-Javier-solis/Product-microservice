3589af27b5249fa24663679b8c37b8e4b4652a63
javiers78889 <javiers78889@gmail.com>
2025-05-19 15:40:05 -0500

    ajustes

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,6 +3,7 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/dto/pagination.dto';
+import { zip } from 'rxjs';
 
 @Controller('products')
 export class ProductsController {
@@ -30,6 +31,6 @@ export class ProductsController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+    return this.productsService.remove(+id); //comentario
   }
 }
